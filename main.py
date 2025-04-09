3eeb697631acf7e5a39c77d0d157e6ed35faf1c5
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-04-10 00:33:32 +0300

    base html and main

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'lawdpawlkDPKAW0DKA0kygOYUGyvbyVYVyvBGUHIjijw'
+
+
+@app.route('/test')
+def test():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='0.0.0.0')
