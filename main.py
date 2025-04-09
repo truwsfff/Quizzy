16e648e6cc5d9438a01109377adbb40e2ac9927f
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-04-10 00:37:25 +0300

    base html and main (fixes)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'lawdpawlkDPKAW0DKA0kygOYUGyvbyVYVyvBGUHIjijw'
+app.config['SECRET_KEY'] = ''
 
 
 @app.route('/test')
