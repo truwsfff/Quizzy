bf4fbc18cf584acacfded60688ae648ae6fd66a8
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-04-10 23:59:24 +0300

    starting work on sessions

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, session
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ''
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-@app.route('/test')
+@app.route('/')
 def test():
     return render_template('base.html')
 
